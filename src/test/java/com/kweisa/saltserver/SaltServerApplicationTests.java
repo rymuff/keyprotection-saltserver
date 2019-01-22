0c5f26032ab6b9eac3ad5baee3b500af34b7db31
Junsung Cho <rymuff@gmail.com>
2019-01-22 14:05:28 +0900

    Update

---
+++ src/test/java/com/kweisa/saltserver/SaltServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kweisa.saltserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SaltServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
